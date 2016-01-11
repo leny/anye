33115d95315793cc6b0989b883d9b49c4ef5fa34
leny <info@flatland.be>
2016-01-11 13:05:49 +0100

    Rewrite grunt tasks & update dependencies

---
+++ Gruntfile.js
@@ -0,0 +1,48 @@
+"use strict";
+
+module.exports = ( grunt ) => {
+
+    require( "load-grunt-tasks" )( grunt );
+
+    grunt.initConfig( {
+        "eslint": {
+            "src": [ "src/**/*.js" ]
+        },
+        "babel": {
+            "options": {
+                "presets": [ "es2015" ]
+            },
+            "src": {
+                "files": [ {
+                    "expand": true,
+                    "cwd": "src/",
+                    "src": [ "**/*.js" ],
+                    "dest": "lib/"
+                } ]
+            }
+        },
+        "nodeunit": {
+            "files": [ "test/**/*-test.js" ]
+        },
+        "watch": {
+            "src": {
+                "files": "src/**/¨.js",
+                "tasks": [ "default" ]
+            }
+        }
+    } );
+
+    grunt.registerTask( "test", [ "nodeunit" ] );
+
+    grunt.registerTask( "default", [
+        "eslint",
+        "babel",
+        "test"
+    ] );
+
+    grunt.registerTask( "work", [
+        "default",
+        "watch"
+    ] );
+
+};
